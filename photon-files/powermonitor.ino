129922a9b6bd75f0b7f72420f718a623275be0e7
NoahEFaust <noah@fausthouse.org>
2016-04-05 13:53:57 -0400

    Update powermonitor.ino
    Basic firmware with publish.variable

--- photon-files/powermonitor.ino
+++ photon-files/powermonitor.ino
@@ -2,11 +2,11 @@
 int analogvals[2500];
 int cnt;
 float Vmid = 1.61;
-float RMScurrent;
+double RMScurrent;
 char publishString[40];
 
 void setup() {
-
+Particle.variable("RMScurrent", &RMScurrent, DOUBLE);
 }
 
 void loop() {
@@ -15,7 +15,7 @@ void loop() {
 //very quickly sample the signal
 cnt = 0;
 
-while(cnt<1000){
+while(cnt<2500){
     
     analogvals[cnt] = analogRead(A0); ///store current read to the array location cnt
     cnt = cnt + 1;
@@ -27,14 +27,14 @@ cnt = 0;
 RMScurrent = 0;
 
 
-while(cnt<1000){
+while(cnt<2500){
     
     RMScurrent = RMScurrent + pow((3.3*((float) analogvals[cnt])/4095-Vmid)*200,2); ///store current read to the array location cnt
     cnt = cnt + 1;
     
 }
 
-RMScurrent = sqrt(RMScurrent/10000);
+RMScurrent = sqrt(RMScurrent/2500);
 
 sprintf(publishString, "%lf", RMScurrent);
 Particle.publish("RMScurrent",publishString);
